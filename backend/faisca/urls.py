e2968e2d1ff23ec3a31402c502ac398e807a1066
Andre Dias <andrefonsecadias21@gmail.com>
2025-03-30 22:47:25 -0300

    Criação do Agente de Ia Faisca Agente

--- backend/faisca/urls.py
+++ backend/faisca/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('chat', views.chatbot, name='chatbot'),
     path('history', views.chat_history, name='chat_history'),
     path('reset_chat', views.reset_chat, name='reset_chat'),  # Adicione esta linha
-]
+    path('system_agent', views.system_agent, name='system_agent'),
+    path('faisca_agent/chat', views.faisca_agent_chat, name='faisca_agent_chat'),
+    path('faisca_agent/reset', views.reset_faisca_agent_chat, name='reset_faisca_agent_chat'),
+    ]
