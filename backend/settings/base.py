ce8107c7a86eaaefd95fd3cf6fb6518f49408078
Andre-fdias <andrefonsecadias21@gmail.com>
2025-11-11 15:52:26 -0300

    fix(settings): Remove a opção CLIENT_CLASS do cache Redis

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -255,11 +255,8 @@ SECURE_SSL_REDIRECT = False
 # ============ CONFIGURAÇÕES DE CACHE ============
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',  # CORRIGIDO
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
     }
 }
 
