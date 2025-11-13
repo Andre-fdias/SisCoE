458bc11434f025f7a38163683f3fd1c7f3100e56
Andre-fdias <andrefonsecadias21@gmail.com>
2025-11-13 12:20:14 -0300

    feat(control_panel, core): Adiciona migrações e comando de superusuário
    - Adiciona migrações iniciais para o aplicativo 'control_panel', refletindo novas estruturas de modelo.
    
    - Inclui um novo comando de gerenciamento 'create_dev_superuser' para facilitar a configuração do ambiente de desenvolvimento.

---
+++ backend/core/management/commands/create_dev_superuser.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    """
+    Cria um superusuário para o ambiente de desenvolvimento se ele não existir.
+    """
+    help = 'Cria um superusuário padrão para desenvolvimento.'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        email = 'admin@siscoe.com'
+        password = 'bombeiros'
+        
+        if not User.objects.filter(email=email).exists():
+            self.stdout.write(self.style.SUCCESS(f'Criando superusuário de desenvolvimento com email {email}...'))
+            User.objects.create_superuser(username=email, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superusuário criado com sucesso!'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Superusuário com email {email} já existe.'))
+
